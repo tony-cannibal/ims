e1537b94ed1c48c756fe8fbce40931bafbadba2b
tony-cannibal <castrovital.luis@gmail.com>
2023-09-30 17:08:41 -0600

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import tkinter
+from ttkbootstrap import Style
+from tkinter import ttk
+
+
+class Application(tkinter.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('File Search Engine')
+        self.style = Style('journal')
+        self.login = Login(self, "Hello There.")
+        self.login.pack()
+
+
+class Login(ttk.Frame):
+    def __init__(self, parent, string):
+        super().__init__(parent)
+
+        self.label_1 = ttk.Label(self, text=string, padding=10)
+        self.label_1.grid(row=0, column=0)
+        self.padding = 10
+
+        self.button_1 = ttk.Button(self, text="Im a Button")
+        self.button_1.grid(row=1, column=0)
+
+
+if __name__ == '__main__':
+    Application().mainloop()
