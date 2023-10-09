64fe1b91532ea59a316206abbbf8b864295428f1
tony-cannibal <castrovital.luis@gmail.com>
2023-10-09 12:06:59 -0600

    Finished the inventory general UI

---
+++ Inventory.py
@@ -0,0 +1,109 @@
+import tkinter as tk
+from tkinter import font
+# from ttkbootstrap import Style
+from tkinter import ttk
+import constants as cn
+
+
+class Inventory(ttk.Frame):
+    def __init__(self, parent, changeGeometry):
+        super().__init__(parent)
+
+        ######################################
+        # Data Capture
+        self.grid_columnconfigure(
+            (0, 1, 2, 3, 4, 5), weight=1, uniform="column")
+        self.changeSize = changeGeometry
+
+        self.label_1 = ttk.Label(
+            self, text="INVENTARIO DE LOTES FEEDER", font="arial 14")
+        self.label_1.grid(row=0, column=0, columnspan=6)
+        customFont = font.Font(family="Arial", size=14)
+
+        self.captura = ttk.Entry(self, font=customFont, width=50)
+        self.captura.grid(row=1, column=0, sticky="EW",
+                          pady=20, columnspan=4)
+
+        # self.combo = ttk.Combobox(self, width=8)
+        # self.combo.grid(row=1, column=3, columnspan=1, padx=(15, 0), ipadx=10)
+
+        self.resetButton = tk.Button(
+            self, text="Opciones", font=customFont,
+            command=lambda: self.changeSize("inventory"))
+        self.resetButton.grid(row=1, column=4, padx=10, sticky="EW")
+
+        self.configButton = tk.Button(
+            self, text="Configuracion", font=customFont,
+            command=lambda: self.changeSize("other"))
+        self.configButton.grid(row=1, column=5, sticky="EW")
+
+        #######################################
+        # Labels
+
+        self.statusFrame = ttk.LabelFrame(self, text="Status")
+        self.statusFrame.grid(column=0, row=4, columnspan=6, sticky="EW")
+
+        self.statusFrame.grid_columnconfigure(
+            (0, 1, 2), weight=1, uniform="column")
+
+        self.label_sap = tk.Label(
+            self.statusFrame, text="SAP", **cn.labelConf)
+        self.label_sap.grid(row=0, column=0, sticky="NSEW", padx=(6, 0))
+        self.labelSapAmount = tk.Label(
+            self.statusFrame, text="0", **cn.labelConf)
+        self.labelSapAmount.grid(row=1, column=0, sticky="NSEW", padx=(6, 0))
+
+        self.label_inventario = tk.Label(
+            self.statusFrame, text="INVENTARIO", **cn.labelConf)
+        self.label_inventario.grid(row=0, column=1, padx=10, sticky="NSEW")
+
+        self.labelInventarioAmount = tk.Label(
+            self.statusFrame, text="0", **cn.labelConf)
+        self.labelInventarioAmount.grid(
+            row=1, column=1, padx=10, sticky="NSEW")
+
+        self.label_porcentaje = tk.Label(
+            self.statusFrame, text="PORCENTAJE DE INVENTARIO",
+            wraplength=110, justify="center", **cn.labelConf)
+        self.label_porcentaje.grid(row=0, column=2, sticky="NSEW", padx=(0, 6))
+
+        self.labelPorcentajeAmount = tk.Label(
+            self.statusFrame, text="0",
+            wraplength=110, justify="center", **cn.labelConf)
+        self.labelPorcentajeAmount.grid(
+            row=1, column=2, sticky="NSEW", padx=(0, 6))
+
+        ###############################################
+        # Tables
+
+        self.tabWidget = ttk.Notebook(self.statusFrame)
+
+        self.progressTab = ttk.Frame(self.tabWidget)
+        self.missingTab = ttk.Frame(self.tabWidget)
+
+        self.tabWidget.add(self.progressTab, text="Escaneado")
+        self.tabWidget.add(self.missingTab, text="Faltantes")
+
+        self.history = ttk.Treeview(self.progressTab,
+                                    columns=cn.columnHeadigns, show="headings")
+        for i in range(4):
+            self.history.heading(
+                cn.columnHeadigns[i], text=cn.columnHeadigns[i])
+        for i in range(4):
+            self.history.column(
+                cn.columnHeadigns[i], width=cn.columnWith[i], anchor="center")
+
+        self.history.pack(expand=1, fill="both")
+
+        self.faltantes = ttk.Treeview(self.missingTab,
+                                      columns=cn.columnHeadigns, show="headings")
+        for i in range(4):
+            self.faltantes.heading(
+                cn.columnHeadigns[i], text=cn.columnHeadigns[i])
+        for i in range(4):
+            self.faltantes.column(
+                cn.columnHeadigns[i], width=cn.columnWith[i], anchor="center")
+        self.faltantes.pack(expand=1, fill="both")
+
+        self.tabWidget.grid(column=0, row=2, columnspan=3,
+                            sticky="EW", pady=5, padx=5)
--- main.py
+++ main.py
@@ -1,75 +1,27 @@
 import tkinter as tk
-from tkinter import font
+# from tkinter import font
 from ttkbootstrap import Style
-from tkinter import ttk
-import constants as cn
+# from tkinter import ttk
+# import constants as cn
+
+from Inventory import Inventory
 
 
 class Application(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Inventario Feeder')
-        self.geometry('800x500')
+        self.geometry('800x520')
         self.resizable(False, False)
-        self.style = Style('journal')
+        self.style = Style('darkly')
         self.Inventory = Inventory(self, self.changeGeometry)
         self.Inventory.pack(padx=20, pady=10)
 
     def changeGeometry(self, window):
         if window == "inventory":
-            self.geometry('800x500')
+            self.geometry('800x520')
         else:
             self.geometry('800x800')
-
-
-class Inventory(ttk.Frame):
-    def __init__(self, parent, changeGeometry):
-        super().__init__(parent)
-
-        self.grid_columnconfigure(
-            (0, 1, 2, 3, 4, 5), weight=1, uniform="column")
-        self.changeSize = changeGeometry
-
-        self.label_1 = ttk.Label(
-            self, text="INVENTARIO DE LOTES FEEDER", font="arial 14")
-        self.label_1.grid(row=0, column=0, columnspan=6)
-        customFont = font.Font(family="Arial", size=14)
-
-        self.captura = ttk.Entry(self, font=customFont, width=50)
-        self.captura.grid(row=1, column=0, sticky="EW",
-                          pady=20, padx=(0, 10), columnspan=4)
-
-        # self.combo = ttk.Combobox(self, width=8)
-        # self.combo.grid(row=1, column=3, columnspan=1, padx=(15, 0), ipadx=10)
-
-        self.resetButton = ttk.Button(
-            self, text="Reset", command=lambda: self.changeSize("inventory"))
-        self.resetButton.grid(row=1, column=4, padx=10)
-
-        self.configButton = ttk.Button(
-            self, text="config", command=lambda: self.changeSize("other"))
-        self.configButton.grid(row=1, column=5)
-
-        #######################################
-
-        self.statusFrame = ttk.LabelFrame(self, text="Status")
-        self.statusFrame.grid(column=0, row=4, columnspan=6, sticky="EW")
-
-        self.statusFrame.grid_columnconfigure(
-            (0, 1, 2), weight=1, uniform="column")
-
-        self.label_sap = ttk.Label(
-            self.statusFrame, text="SAP", font=cn.fontLable)
-        self.label_sap.grid(row=0, column=0)
-
-        self.label_inventario = ttk.Label(
-            self.statusFrame, text="INVENTARIO", font=cn.fontLable)
-        self.label_inventario.grid(row=0, column=1, padx=10)
-
-        self.label_porcentaje = ttk.Label(
-            self.statusFrame, text="PORCENTAJE DE INVENTARIO",
-            wraplength=110, justify="center", font=cn.fontLable)
-        self.label_porcentaje.grid(row=0, column=2, )
 
 
 if __name__ == '__main__':
